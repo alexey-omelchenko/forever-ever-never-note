1265ad1f13ed56ed769ba1e6eb63062baff51198
Thor Chen <thorchen@catalyst.net.nz>
2019-06-14 14:54:24 +1200

    FIX: deployment issue

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import './index.scss'
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('./service-worker.js')
       .then(registration => {
         console.log('SW registered: ', registration)
       })
